15e541b07c8729e6510250feaed28ce27d8c48c2
aripramuja <105218009@student.universitaspertamina.ac.id>
2021-07-14 20:24:30 +0700

    update schema

--- src/validator/schema.js
+++ src/validator/schema.js
@@ -7,8 +7,8 @@ const MusicPayloadSchema = Joi.object({
   title: Joi.string().required(),
   year: Joi.number().required(),
   performer: Joi.string().required(),
-  genre: Joi.string().required(),
-  duration: Joi.number().required(),
+  genre: Joi.string(),
+  duration: Joi.number(),
 });
 
 module.exports = { MusicPayloadSchema };
