92748a753552a38c286cb972683b30f47c8b8b7d
aripramuja <105218009@student.universitaspertamina.ac.id>
2021-07-26 08:21:45 +0700

    update server & index collaboration

--- src/api/authentications/index.js
+++ src/api/authentications/index.js
@@ -4,8 +4,8 @@ const routes = require("./routes");
 module.exports = {
   name: "authentications",
   version: "1.0.0",
-  register: async (server, { authenticationsService, usersService, tokenManager, validator }) => {
-    const authenticationsHandler = new AuthenticationsHandler(authenticationsService, usersService, tokenManager, validator);
+  register: async (server, { service, usersService, tokenManager, validator }) => {
+    const authenticationsHandler = new AuthenticationsHandler(service, usersService, tokenManager, validator);
     server.route(routes(authenticationsHandler));
   },
 };
--- src/api/collaborations/index.js
+++ src/api/collaborations/index.js
@@ -4,8 +4,8 @@ const CollaborationsHandler = require("./handler");
 module.exports = {
   name: "collaborations",
   version: "1.0.0",
-  register: async (server, { collaborationsService, playlistsService, validator }) => {
-    const collaborationsHandler = new CollaborationsHandler(collaborationsService, playlistsService, validator);
+  register: async (server, { service, playlistsService, validator }) => {
+    const collaborationsHandler = new CollaborationsHandler(service, playlistsService, validator);
     server.route(routes(collaborationsHandler));
   },
 };
